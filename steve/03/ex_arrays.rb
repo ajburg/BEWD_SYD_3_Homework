473070682f4de64f230dfb7422245c8c2301568c
Stephen Lead <stephen.lead@gmail.com>
2014-08-27 21:04:36 +1000

    lesson

--- steve/03/ex_arrays.rb
+++ steve/03/ex_arrays.rb
@@ -44,28 +44,28 @@ end
 
 
 # Create an empty array named categories using a literal
-categories = ___
+categories = []
 assert_equal categories, []
 
 # Create an empty array named categories using Array's #new method
-categories = ___
+categories = Array.new
 assert_equal categories, []
 
 # Add 3 stories to the categories array. Each time using a different method.
-categories.___("Music")
-categories.___("Weather")
-categories.___("Florida")
+categories.push("Music")
+categories.unshift("Weather")
+categories<< "Florida"
 assert_equal ["Florida", "Music", "Weather"], categories.sort
 
 # Ensure only unique categories get stored
 categories << "Florida"
-categories.___!
+categories.pop
 assert_equal ["Florida", "Music", "Weather"], categories.sort
 
 # Write a conditional that adds "Family" to the category list if it includes both Animals, and Shopping
 categories << "Animals"
 
-if ___
+if categories.include? "Animals" and categories.include? "Shopping"
   categories << "Family"
 end
 
@@ -74,7 +74,7 @@ assert_equal(false, categories.include?("Family"))
 categories << "Shopping"
 
 #repeat check here
-if ___
+if categories.include? "Animals" and categories.include? "Shopping"
   categories << "Family"
 end
 
--- steve/03/ex_loop.rb
+++ steve/03/ex_loop.rb
@@ -1,9 +1,51 @@
 # Write a program that prints 99 bottles of beer on the wall.
 # The song starts with
-# 99 bottles of beer on the wall
-# 99 bottles of beer!
-# You take one down and pass it around,
-# 98 bottles of beer on the wall!
+
+def writeLine(idx)
+
+	puts "\n"
+	puts idx.to_s + " bottles of beer on the wall"
+	puts idx.to_s + " bottles of beer!"
+	puts "You take one down and pass it around,"
+	idx -= 1
+	if idx < 1
+		puts "No more bottles of beer on the wall"
+	elsif idx == 1
+		puts "Only one bottle of beer on the wall"
+	else
+		puts idx.to_s + " bottles of beer on the wall!"
+	end
+	return idx
+end
+
+idx = 99
+while idx > 0
+	idx = writeLine(idx) 
+end
+
+num = 99
+#num.downto(1) do |idx|
+#	puts "\n"
+#	puts "#{idx} bottles of beer on the wall"
+#	puts "#{idx} bottles of beer!"
+#	puts "you take one down and pass it around"
+#	idx2 = idx - 1
+#	puts "#{idx2} bottles of beer on the wall"
+#	
+#end
+
+#num.times do |x|
+#	idx = 99 - x
+#	puts "\n"
+#	puts "#{idx} bottles of beer on the wall"
+#	puts "#{idx} bottles of beer!"
+#	puts "you take one down and pass it around"
+#	idx2 = idx - 1
+#	puts "#{idx2} bottles of beer on the wall"
+#	
+#	
+#end
+
 #
 # And ends with
 # 1 bottle of beer on the wall
