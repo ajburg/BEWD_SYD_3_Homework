bc268ac6f2f2c0e583d00d7fd809e6fa14815cf6
Stephen Lead <stephen.lead@gmail.com>
2014-08-27 22:29:49 +1000

    replaced tabs with spaces in teddit script

--- steve/03/ex_teddit_hashes.rb
+++ steve/03/ex_teddit_hashes.rb
@@ -18,22 +18,22 @@
 
 
 def calculate_upvotes(title, category)
-	# Write code so that:
-		# If the Story is about cats multiply the upvotes by 5
-		# If the Story is about bacon multiply the upvotes by 8
-		# If the Story is about Food it gets 3 times the upvotes.
+  # Write code so that:
+    # If the Story is about cats multiply the upvotes by 5
+    # If the Story is about bacon multiply the upvotes by 8
+    # If the Story is about Food it gets 3 times the upvotes.
 
-	#For example:
-	# "Cats frolic despite tuna shortage" should give you 5 times the upvotes!
-	if title.include? 'cats'
-		upvotes = 5
-	elsif
-		title.include? 'bacon'
-		upvotes = 8
-	else
-		upvotes = 1
-	end
-	return upvotes
+  #For example:
+  # "Cats frolic despite tuna shortage" should give you 5 times the upvotes!
+  if title.include? 'cats'
+    upvotes = 5
+  elsif
+    title.include? 'bacon'
+    upvotes = 8
+  else
+    upvotes = 1
+  end
+  return upvotes
 end
 
 #Initialise the story collection with the starter story
@@ -43,17 +43,17 @@ puts "Welcome to Teddit! a text based news aggregator. Get today's news tomorrow
 
 #Keep looping until the user enters a blank line
 while true
-	puts "\nPlease enter a new story (blank to cancel):"
-	title = gets.chomp
-	break if title == ""
-	puts "Please give it a category:"
-	category = gets.chomp
-	upvotes = calculate_upvotes(title, category)
-	story = {title: title, category: category, votes: upvotes}
-	stories.push(story)
+  puts "\nPlease enter a new story (blank to cancel):"
+  title = gets.chomp
+  break if title == ""
+  puts "Please give it a category:"
+  category = gets.chomp
+  upvotes = calculate_upvotes(title, category)
+  story = {title: title, category: category, votes: upvotes}
+  stories.push(story)
 end
 
 puts "Current stories are:"
 stories.each do |story, category, votes|
-	puts "Story: #{story[:title]} in Category: #{story[:category]} (#{story[:votes]} votes)"
+  puts "Story: #{story[:title]} in Category: #{story[:category]} (#{story[:votes]} votes)"
 end
