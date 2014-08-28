9fe00725a3e033cd26c92de1e559ed82c48861c5
Stephen Lead <stephen.lead@gmail.com>
2014-08-28 10:11:24 +1000

    fixed errors in ex_teddit script

--- steve/03/ex_teddit_hashes.rb
+++ steve/03/ex_teddit_hashes.rb
@@ -1,59 +1,41 @@
 # Step 1: Working with Arrays and Hashes
 
-# Copy your solution from ex_teddit_conditional.rb in 02_Variables_Conditionals
-# Create an empty stories array. This will be used to hold all stories we capture.
-# Users enter the story as they do now, but it gets put in a hash instead. 
-# Update any reference to the story (upvotes, category and title) 
-# Your story hash should look like the one below: 
-# { title: "Monkeys thank mayor for flounder tooth necklace", category: "Teeth", upvotes: 1 }
-# Add the hash to an array called stories and print "Story: Monkeys thank mayor for flounder tooth necklace, Category: (Teeth), Current Upvotes: 1"
-# Using the stories array 
-# Test your cat, bacon, and food upvote conditional logic.
-#
-# Step 2: Adding Loops 
-# Use a loop, so that your program continues to ask a user for stories until they chose to quit. ("Would you like to add another story? Enter 'y' or 'n'")
-# Once the user is finished with entering their stories, use .each to print each story in the stories array. 
-#
-#
-
-
-def calculate_upvotes(title, category)
+def calculate_upvotes(title)
   # Write code so that:
-    # If the Story is about cats multiply the upvotes by 5
-    # If the Story is about bacon multiply the upvotes by 8
-    # If the Story is about Food it gets 3 times the upvotes.
+  # If the Story is about cats multiply the upvotes by 5
+  # If the Story is about bacon multiply the upvotes by 8
+  # If the Story is about Food it gets 3 times the upvotes.
 
-  #For example:
+  # For example:
   # "Cats frolic despite tuna shortage" should give you 5 times the upvotes!
+  upvotes = 1
   if title.include? 'cats'
     upvotes = 5
-  elsif
-    title.include? 'bacon'
+  elsif title.include? 'bacon'
     upvotes = 8
-  else
-    upvotes = 1
   end
   return upvotes
 end
 
-#Initialise the story collection with the starter story
-stories = [{ title: "Monkeys thank mayor for flounder tooth necklace", category: "Teeth", votes: 1 }]
+# Initialise the story collection with the starter story
+stories = [{ title: 'Monkeys thank mayor for flounder tooth necklace',
+  category: 'Teeth', votes: 1 }]
 
-puts "Welcome to Teddit! a text based news aggregator. Get today's news tomorrow!"
+puts 'Welcome to Teddit! Get today\'s news tomorrow!'
 
-#Keep looping until the user enters a blank line
+# Keep looping until the user enters a blank line
 while true
-  puts "\nPlease enter a new story (blank to cancel):"
+  puts 'Please enter a new story (blank to cancel):'
   title = gets.chomp
-  break if title == ""
-  puts "Please give it a category:"
-  category = gets.chomp
-  upvotes = calculate_upvotes(title, category)
-  story = {title: title, category: category, votes: upvotes}
-  stories.push(story)
+  break if title == ''
+  puts 'Please give it a category:'
+  a_category = gets.chomp
+  upvotes = calculate_upvotes(title)
+  a_story = { title: title, category: a_category, votes: upvotes }
+  stories.push(a_story)
 end
 
-puts "Current stories are:"
-stories.each do |story, category, votes|
+puts 'Current stories are:'
+stories.each do |story|
   puts "Story: #{story[:title]} in Category: #{story[:category]} (#{story[:votes]} votes)"
 end
