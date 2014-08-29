769cc06117668ed894fff037f9d63a0daab34c2c
Stephen Lead <stephen.lead@gmail.com>
2014-08-29 10:45:33 +1000

    now builds the stories array correctly. next need to calculate votes

--- steve/04/ex_teddit_api_news.rb
+++ steve/04/ex_teddit_api_news.rb
@@ -1,8 +1,4 @@
 #!/usr/bin/env ruby
-# We're going to add a remote data source to pull in stories from Mashable and Digg.
-  # http://mashable.com/stories.json
-  # http://digg.com/api/news/popular.json
-# These stories will also be upvoted based on our rules. No more user input!
 
 # Pull the json, parse it and then make a new story hash out of each story(Title, Category, Upvotes)
 # Add each story to an array and display your "Front page"
@@ -34,8 +30,8 @@ def show_all_stories(stories)
   end
 end
 
-def construct_story_hash(title, category, source)
-  { :title => title, :category => category, :upvotes => 0, :source => source }
+def construct_story_hash(title, category, source, content)
+  { :title => title, :category => category, :upvotes => 0, :source => source, :content => content}
 end
 
 def process_mashable_stories(stories)
@@ -43,35 +39,28 @@ def process_mashable_stories(stories)
   json_data = JSON.parse(data)
   keys = json_data.keys
   keys.each do |key|
-    stories = json_data[key]
-    if stories != nil
-      puts
-      puts key.upcase + ':'
-      puts
-      stories.each do |story|
-        title = story['title'].upcase
-        author = story['author']
-        content = story['content']['plain']
-        puts title + ' (by ' + author + ')'
-        puts truncate(content)
+    articles = json_data[key]
+    if articles != nil
+      articles.each do |article|
+        title = article['title'].upcase
+        author = article['author']
+        content = article['content']['plain']
+        link = article['link']
+        shares = article['shares']['total']
+        category = article['channel']
+        story = construct_story_hash(title, category, author, content)
+        stories.push(story)
       end
     end
   end
 end
 
-def process_digg_stories(stories)
-  puts stories
-end
-
 def show_all_stories(stories)
   puts stories
 end
+
 stories = []
 
 puts 'Getting stories from Mashable'
 process_mashable_stories(stories)
-
-#puts 'Getting stories from Digg'
-#process_digg_stories(stories)
-
 show_all_stories(stories)
