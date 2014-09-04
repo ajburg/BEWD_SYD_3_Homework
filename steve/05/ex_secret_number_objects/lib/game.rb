c5d388caf3588f4e0b9c4b0b5ed137dce3e7a0d1
Stephen Lead <stephen.lead@gmail.com>
2014-09-04 19:59:23 +1000

    started fleshing out the separate classes and files 2

---
+++ steve/05/ex_secret_number_objects/lib/game.rb
@@ -0,0 +1,17 @@
+require 'lib/person'
+require 'lib/secret_number'
+
+class Game
+# this class will be complex
+# we need to write logic to initialize a new game, and run the game loop
+# we'll want to write a few separate methods for unique tasks so that our
+# code is structured properly
+  def initialize
+    puts 'initialized the game'
+  end
+  
+  def start
+    puts 'started the game'
+  end
+  
+end
---
+++ steve/05/ex_secret_number_objects/lib/secret_number.rb
@@ -0,0 +1,6 @@
+class SecretNumber
+# this class will generate our secret number for us
+  def initialize
+    puts 'in secret number'
+  end
+end
