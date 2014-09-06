5a21061c5581b74d335970da3a206f2f088b54a0
Stephen Lead <stephen.lead@gmail.com>
2014-09-06 10:55:38 +1000

    creates actors, fetches their filmographies from IMDB, and attaches them to each actor.

--- steve/midterm/main.rb
+++ steve/midterm/main.rb
@@ -11,13 +11,18 @@ imdb = Imdb.new
 films = []
 actors = []
 
+#Create some actor objects
 actors << Actor.new('Kevin Bacon')
 actors << Actor.new('Brad Pitt')
 actors << Actor.new('Julia Roberts')
+
+#Fetch their filmographies from IMDB. (If I knew how to program events
+#I'd do this automatically when each actor was created)
 actors.each do |actor|
   imdb.find_films(actor)
-  puts
-  puts actor.name.upcase + ':'
-  puts actor.films
+  actor.films.each do |film|
+    films << film
+  end
 end
 
+puts films
